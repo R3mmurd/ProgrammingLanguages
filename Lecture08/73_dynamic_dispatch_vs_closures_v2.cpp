8781922c315012d9b30ce0e4ccf9728fb8154f39
Alejandro Mujica <aledrums@gmail.com>
2024-04-20 10:31:15 -0400

    Added C++ codes for dynamic dispatch vs closures

---
+++ Lecture08/73_dynamic_dispatch_vs_closures_v2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+class A
+{
+public:
+    virtual bool even(int x) const
+    {
+        std::cout << "Even in A\n";
+        if (x == 0)
+        {
+            return true;
+        }
+
+        return odd(x - 1);
+    }
+
+    bool odd(int x) const
+    {
+        std::cout << "Odd in A\n";
+        if (x == 0)
+        {
+            return false;
+        }
+
+        return even(x - 1);
+    }
+};
+
+class B: public A
+{
+public:
+    bool even(int x) const
+    {
+        std::cout << "Even in B\n";
+        return x % 2 == 0;
+    }
+};
+
+class C: public A
+{
+public:
+    bool even(int x) const
+    {
+        std::cout << "Even in C\n";
+        return false;
+    }
+};
+
+int main()
+{
+    std::cout << std::boolalpha;
+
+    bool a1 = A{}.odd(7);
+    std::cout << "a1 is " << a1 << "\n\n";
+
+    bool a2 = B{}.odd(7);
+    std::cout << "a2 is " << a2 << "\n\n";
+
+    bool a3 = C{}.odd(7);
+    std::cout << "a3 is " << a3 << "\n\n";
+
+    return EXIT_SUCCESS;
+};
